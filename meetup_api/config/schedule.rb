608fe0a62ce6e440c431cd98344427e9e4283f81
Devin Dreszer <devindreszer@gmail.com>
2014-08-18 17:08:11 -0400

    App gathers group count data every 15 minutes

--- meetup_api/config/schedule.rb
+++ meetup_api/config/schedule.rb
@@ -21,6 +21,6 @@
 set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 20.minutes do
+every 15.minutes do
   rake "meetup:group_counts"
 end
