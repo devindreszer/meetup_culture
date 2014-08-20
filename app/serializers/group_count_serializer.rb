bcea9c92952608cd0946ca27b8b0a2fd5c34eebf
Devin Dreszer <devindreszer@gmail.com>
2014-08-20 12:31:35 -0400

    Group Counts include percentages

--- app/serializers/group_count_serializer.rb
+++ app/serializers/group_count_serializer.rb
@@ -1,7 +1,15 @@
 class GroupCountSerializer < ActiveModel::Serializer
-  attributes :city, :group_count
+  attributes :city, :state, :group_count, :group_percentages
 
   def city
     object.city.city
+  end
+
+  def state
+    object.city.state
+  end
+
+  def group_percentages
+    object.group_count.to_f / object.city.total_groups
   end
 end
