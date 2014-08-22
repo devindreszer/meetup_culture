0081a709e0a23abd19d83bd98d0d3e76b6202c90
Devin Dreszer <devindreszer@gmail.com>
2014-08-22 10:04:29 -0400

    More group count data

--- public/app/app.js
+++ public/app/app.js
@@ -13,4 +13,10 @@
       .otherwise({ redirectTo: '/' });
   }]);
 
+  app.filter('percentage', ['$filter', function ($filter) {
+    return function (input, decimals) {
+      return $filter('number')(input * 100, decimals) + '%';
+    };
+  }]);
+
 })();
