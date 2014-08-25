01ec893d4e0264ff515a854abc71e6462bc36ad0
Devin Dreszer <devindreszer@gmail.com>
2014-08-25 12:57:58 -0400

    Initial similar city link diagram

--- public/app/controllers/similarCityLinksController.js
+++ public/app/controllers/similarCityLinksController.js
@@ -4,8 +4,43 @@
 
     function init() {
       similarCityLinksFactory.getSimilarCityLinks()
-        .success(function(similarCityLinks) {
-          $scope.similarCityLinks = similarCityLinks;
+        .success(function(data) {
+          var sources = [],
+            similarCityNodes = [],
+            similarCityLinks = [];
+
+          data.forEach(function(link){
+            sources.push(link.source);
+          });
+          sources = _.uniq(sources);
+          sources = _.sortBy(sources);
+
+          for(var i = 0; i < sources.length; i++) {
+            similarCityNodes.push({
+              city: sources[i],
+              x: (360 / sources.length) * i,
+              y: 360
+            });
+          }
+
+          data.forEach(function(link){
+            sourceNode = similarCityNodes.filter(function(node){
+              return node.city === link.source;
+            });
+            targetNode = similarCityNodes.filter(function(node){
+              return node.city === link.target;
+            });
+
+            similarCityLinks.push({
+              source: sourceNode[0],
+              target: targetNode[0]
+            });
+          });
+
+          $scope.similarCities = {
+            links: similarCityLinks,
+            nodes: similarCityNodes
+          };
         })
         .error(function(data) {
           console.log(data);
