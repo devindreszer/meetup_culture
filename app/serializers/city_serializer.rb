301d64934af6787cb574892953fe1a846b5e1c1f
Devin Dreszer <devindreszer@gmail.com>
2014-08-28 10:05:50 -0400

    City selections include the state

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -1,4 +1,8 @@
 class CitySerializer < ActiveModel::Serializer
-  attributes :city, :state, :total_groups
+  attributes :city, :state, :total_groups, :selection
   has_many :group_counts
+
+  def selection
+    "#{object.city}, #{object.state}"
+  end
 end
